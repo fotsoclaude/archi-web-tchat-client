295a9fea81b70b2bb0c7d0ff9307bb0733e0b807
FOTSO Claude <batfkc@gmail.com>
2022-06-23 11:39:15 +0100

    Routing du module de bienvenue implémenté

--- src/app/connexion/connexion.module.ts
+++ src/app/connexion/connexion.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { RegisterComponent } from './register/register.component';
+import { ConnectionComponent } from './connection/connection.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
   declarations: [
     WelcomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    ConnectionComponent,
+    ConnexionComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule
