8fbcdfc23f015a0a5178436ed59ca0731cc4bae4
FOTSO Claude <batfkc@gmail.com>
2022-06-23 11:40:29 +0100

    Routing du module de bienvenue implémenté

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from "./connexion/login/login.component";
+import { RegisterComponent } from "./connexion/register/register.component";
+import { WelcomeComponent } from "./connexion/welcome/welcome.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: "", component:WelcomeComponent},
+    {path: "register", component:RegisterComponent},
+    {path: "login", component:LoginComponent}
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const RoutingComponent = [WelcomeComponent, RegisterComponent, LoginComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, RoutingComponent } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './connexion/welcome/welcome.component';
@@ -8,7 +8,8 @@ import { WelcomeComponent } from './connexion/welcome/welcome.component';
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    RoutingComponent
   ],
   imports: [
     BrowserModule,
